c6f6124ca23bbb095f1de90198982499d6e30eb5
Ferengi2002 <juan.rengifo@epn.edu.ec>
2024-07-12 09:16:56 -0500

    interfaz lista

--- 01-clases/app/src/main/java/com/example/a2024aswgr2jfrq/MainActivity.kt
+++ 01-clases/app/src/main/java/com/example/a2024aswgr2jfrq/MainActivity.kt
@@ -1,10 +1,73 @@
 package com.example.a2024aswgr2jfrq
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.provider.ContactsContract.CommonDataKinds.Contactables
 import android.widget.Button
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+    
+    fun mostrarSnackBar(texto:String){
+        val snack = Snackbar.make(
+            findViewById(R.id.id_layout_main),
+            texto,
+            Snackbar.LENGTH_INDEFINITE
+        )
+        snack.show()
+    }
+
+    val callbackContenidoIntentExplicito =
+        registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ){
+            result ->
+            if(result.resultCode == Activity.RESULT_OK){
+                if(result.data != null){
+                    //logica negocio
+                    val data = result.data;
+                    mostrarSnackBar(
+                        "${data?.getStringExtra("nombreModificado")}"
+                    )
+                }
+            }
+        }
+
+    val callbackContenidoIntentImplicito =
+        registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ){
+                result ->
+            if(result.resultCode == Activity.RESULT_OK){
+                if(result.data != null){
+                    //logica negocio
+                    if(result.data!!.data != null){
+                        val uri: Uri = result.data!!.data!!
+                        val cursor = contentResolver.query(
+                            uri,null,null,null,null, null
+                        )
+                        cursor?.moveToFirst()
+                        val indiceTelefono = cursor?.getColumnIndex(
+                            ContactsContract.CommonDataKinds.Phone.NUMBER
+                        )
+                        val telefono = cursor?.getString(indiceTelefono!!)
+                        cursor?.close()
+                        mostrarSnackBar("Telefono $telefono")
+
+                    }
+                }
+            }
+        }
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -24,7 +87,38 @@ class MainActivity : AppCompatActivity() {
             .setOnClickListener {
                 irActividad(BListView::class.java)
             }
+
+        val botonIntentImplicito= findViewById<Button>(
+            R.id.btn_ir_intent_implicito
+        )
+        botonIntentImplicito.setOnClickListener {
+            val intentConRespuesta= Intent(
+                Intent.ACTION_PICK,
+                ContactsContract.CommonDataKinds.Phone.CONTENT_URI
+            )
+            callbackContenidoIntentImplicito.launch(intentConRespuesta)
+        }
+        val botonIntentExplicito = findViewById<Button>(
+            R.id.btn_ir_intent_explicito
+        )
+        botonIntentExplicito.setOnClickListener {
+            val intentExplicito = Intent(
+                this,
+                CIntentExplicitoParametros::class.java
+            )
+            intentExplicito.putExtra("nombre","Adrian")
+            intentExplicito.putExtra("apellido","Eguez")
+            intentExplicito.putExtra("edad","34")
+            intentExplicito.putExtra(
+                "entrenador",
+                BEntrenador(10,"Juan","Rengifo")
+            )
+            callbackContenidoIntentExplicito.launch(intentExplicito)
+        }
+
     }
+
+
     fun irActividad(
         clase: Class<*>
     ){
